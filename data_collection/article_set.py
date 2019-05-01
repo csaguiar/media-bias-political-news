c76eaab9ac39e8260e582432bb92f11ab9568f76
Cristiano Aguiar <hello@cristiano.io>
2019-04-30 20:43:47 -0400

    feat: fetch articles from Webhose.io API feat: reusable class `ArticleSet` to store articles and save to csv

---
+++ data_collection/article_set.py
@@ -0,0 +1,46 @@
+import csv
+from datetime import datetime
+
+# Path to file where CSV data, in the format of the
+# Kaggle/Components dataset, is saved
+BASE_CSV_PATH = "data/{}_{}.csv"
+
+
+class ArticleSet:
+    """Class to handle set of articles while fetching from remote."""
+
+    def _filename(self):
+        datetime_now = datetime.now().strftime("%Y-%m-%d_%H:%M")
+        return "data/{}_{}.csv" \
+            .format(self.base_name, datetime_now)
+
+    def save_articles_to_csv(self):
+        """Saves article text and metadata to CSV"""
+        csv_data = [
+            ["title, publication, authors, date, year, month, url, content"]
+        ]
+
+        for article in self.articles:
+            if article.publish_date is not None:
+                publish_date = article.publish_date.strftime("%m-%d-%Y")
+                publish_year = article.publish_date.year
+                publish_month = article.publish_date.month
+            else:
+                publish_date, publish_year, publish_month = None, None, None
+            csv_row = [
+                article.title,
+                article.source_url,
+                article.authors,
+                publish_date,
+                publish_year,
+                publish_month,
+                article.url,
+                article.text
+            ]
+            csv_data.append(csv_row)
+
+        with open(self._filename(), 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(csv_data)
+
+        csv_file.close()
--- data_collection/google_news/scraper.py
+++ data_collection/google_news/scraper.py
@@ -1,17 +1,11 @@
 from bs4 import BeautifulSoup
 from newspaper import Article
-from datetime import datetime
+from data_collection.article_set import ArticleSet
 import requests
 import sys
-import csv
 
 # Default RSS URL from google news for politics topics localized to US
 DEFAULT_URL = "https://news.google.com/rss/topics/CAAqBwgKMPLc8gowgtnZAg?hl=en-US&gl=US&ceid=US:en"
-
-# Path to file where CSV data, in the format of the
-# Kaggle/Components dataset, is saved
-CSV_PATH = "data/google_news_{}.csv" \
-                .format(datetime.now().strftime("%Y-%m-%d_%H:%M"))
 
 
 # From: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
@@ -27,10 +21,11 @@ def progress(count, total, status=''):
     sys.stdout.flush()
 
 
-class ScrapeGoogleNews:
+class ScrapeGoogleNews(ArticleSet):
     """Scrape Google News RSS to save article texts"""
 
     def __init__(self, *args, **kwargs):
+        self.base_name = "google_news"
         self.url = kwargs.get("url", DEFAULT_URL)
         # List of URL for each news article
         self.links = []
@@ -71,34 +66,3 @@ class ScrapeGoogleNews:
                 count += 1
             except:
                 print("Error fetching {}".format(link))
-
-    def save_articles_to_csv(self):
-        """Saves article text and metadata to CSV"""
-        csv_data = [
-            ["title, publication, authors, date, year, month, url, content"]
-        ]
-
-        for article in self.articles:
-            if article.publish_date is not None:
-                publish_date = article.publish_date.strftime("%m-%d-%Y")
-                publish_year = article.publish_date.year
-                publish_month = article.publish_date.month
-            else:
-                publish_date, publish_year, publish_month = None, None, None
-            csv_row = [
-                article.title,
-                article.source_url,
-                article.authors,
-                publish_date,
-                publish_year,
-                publish_month,
-                article.url,
-                article.text
-            ]
-            csv_data.append(csv_row)
-
-        with open(CSV_PATH, 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerows(csv_data)
-
-        csv_file.close()
