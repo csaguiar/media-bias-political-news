4c3c3b20b943ad10a6f0ca34cc4121713e8e4568
Cristiano Aguiar <cristiano.aguiar@avail.app>
2019-05-09 19:17:26 -0400

    feat:  improvements on the webhose script

--- task_webhose.py
+++ task_webhose.py
@@ -1,5 +1,6 @@
 """Fetches articles from Webhose API and saves to a set of CSV Files"""
 import os
+import sys
 from data_collection.webhose.scraper import WebhoseScrape
 
 liberal_news = [
@@ -33,10 +34,23 @@ conservative_news = [
 
 if __name__ == '__main__':
     api_key = os.environ['WEBHOSE_API_KEY']
-    for domain in liberal_news + conservative_news:
+
+    only_politics = False
+    for arg in sys.argv:
+        if arg == "--only-politics":
+            only_politics = True
+
+    if only_politics:
         print("============================================================")
-        print("FETCHING {}".format(domain))
+        print("FETCHING ONLY POLITICS")
         print("============================================================")
-        base_name = "webhose_{}".format(domain)
-        sc = WebhoseScrape(token=api_key, base_name=base_name)
-        sc.fetch_articles(num_pages=2, only_politics=False, domain=domain)
+        sc = WebhoseScrape(token=api_key, base_name="only-politics")
+        sc.fetch_articles(num_pages=100, only_politics=True)
+    else:
+        for domain in liberal_news + conservative_news:
+            print("============================================================")
+            print("FETCHING PREDEFINED SOURCE: {}".format(domain))
+            print("============================================================")
+            base_name = "webhose_{}".format(domain)
+            sc = WebhoseScrape(token=api_key, base_name=base_name)
+            sc.fetch_articles(num_pages=2, only_politics=False, domain=domain)
