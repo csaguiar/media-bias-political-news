611a8ce5bde50c6fb32f42a52c4a3ba4fd42f478
Cristiano Aguiar <hello@cristiano.io>
2019-05-16 11:47:21 -0400

    chore: update webhose articles feat: adding new sources to task_webhose.py script

--- task_webhose.py
+++ task_webhose.py
@@ -1,5 +1,6 @@
 """Fetches articles from Webhose API and saves to a set of CSV Files"""
 import os
+import sys
 from data_collection.webhose.scraper import WebhoseScrape
 
 liberal_news = [
@@ -33,12 +34,33 @@ conservative_news = [
     "dailycaller.com"
 ]
 
+central = [
+    "reuters.com",
+    "csmonitor.com",
+    "fivethirtyeight.com",
+    "bbc.com",
+    "forbes.com",
+]
+
 if __name__ == '__main__':
     api_key = os.environ['WEBHOSE_API_KEY']
-    for domain in liberal_news + conservative_news:
-        print("============================================================")
-        print("FETCHING {}".format(domain))
-        print("============================================================")
-        base_name = "webhose_{}".format(domain)
-        sc = WebhoseScrape(token=api_key, base_name=base_name)
-        sc.fetch_articles(num_pages=2, only_politics=False, domain=domain)
+
+    only_politics = False
+    for arg in sys.argv:
+        if arg == "--only-politics":
+            only_politics = True
+
+    if only_politics:
+        print("==========================================================")
+        print("FETCHING ONLY POLITICS")
+        print("==========================================================")
+        sc = WebhoseScrape(token=api_key, base_name="only-politics")
+        sc.fetch_articles(num_pages=100, only_politics=True)
+    else:
+        for domain in liberal_news + conservative_news + central:
+            print("==========================================================")
+            print("FETCHING PREDEFINED SOURCE: {}".format(domain))
+            print("==========================================================")
+            base_name = "webhose_{}".format(domain)
+            sc = WebhoseScrape(token=api_key, base_name=base_name)
+            sc.fetch_articles(num_pages=3, only_politics=False, domain=domain)
