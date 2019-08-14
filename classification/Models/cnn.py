f8116c689608912d814863a860cff6365777f6af
Cristiano Aguiar <hello@cristiano.io>
2019-08-13 21:08:09 -0400

    fix: missing file in previous commit

--- classification/Models/cnn.py
+++ classification/Models/cnn.py
@@ -14,11 +14,12 @@ from sklearn.model_selection import train_test_split
 import os
 import sys
 import nltk
-nltk.download('stopwords')
-STOPWORDS = nltk.corpus.stopwords.words('english')
+nltk.download("stopwords")
+STOPWORDS = nltk.corpus.stopwords.words("english")
 MODEL_NAME = "cnn"
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 LOGS_FOLDER = "logs"
+
 
 def load_embeddings(embedding_file):
     # Loads embeddings file and dicitionary
@@ -27,10 +28,12 @@ def load_embeddings(embedding_file):
 
     return data["dictionary"], data["reversed_dictionary"], data["embeddings"]
 
+
 def load_dataset(dataset_file):
     dataset = pd.read_csv(dataset_file)
     # Removing neutral articles
     return dataset[dataset["label"] >= 0]
+
 
 def build_sequence(text, dictionary):
     sequence = []
@@ -40,8 +43,9 @@ def build_sequence(text, dictionary):
                 sequence.append(dictionary[token])
             else:
                 sequence.append(0)
-            
-    return sequence    
+
+    return sequence
+
 
 def process_data(data, dictionary, length_threshold=1500):
     data["sequences"] = data["content"].apply(lambda t: build_sequence(t, dictionary))
@@ -51,42 +55,84 @@ def process_data(data, dictionary, length_threshold=1500):
     X = data["sequences"].values
     y = data["label"].values
 
-    X = pad_sequences(X, padding='post', maxlen=max_length)
+    X = pad_sequences(X, padding="post", maxlen=max_length)
 
     return X, y
 
-def train(X, y, embeddings, model_params, test_size=0.25):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=test_size)
-    vocab_size, embeddings_size = embeddings.shape
-    max_length = X.shape[1]
-    print(max_length)
+
+def create_model(embeddings, model_params):
+    max_length = model_params.get("max_length")
+    vocab_size = model_params.get("vocab_size")
+    embeddings_size = model_params.get("embeddings_size")
+    n_filters_1 = model_params.get("n_filters_1")
+    n_filters_2 = model_params.get("n_filters_2")
+    kernel_size_1 = model_params.get("kernel_size_1")
+    kernel_size_2 = model_params.get("kernel_size_2")
+    print(model_params)
     model = Sequential(
         [
-            Embedding(vocab_size, embeddings_size, weights=[embeddings], input_length=max_length, trainable=False),
-            Conv1D(filters=64, kernel_size=4, activation='relu'),
+            Embedding(
+                vocab_size,
+                embeddings_size,
+                weights=[embeddings],
+                input_length=max_length,
+                trainable=False
+            ),
+            Conv1D(
+                filters=n_filters_1,
+                kernel_size=kernel_size_1,
+                activation="relu"
+            ),
             MaxPooling1D(pool_size=3),
-            Conv1D(filters=64, kernel_size=4, activation='relu'),
+            Conv1D(
+                filters=n_filters_2,
+                kernel_size=kernel_size_2,
+                activation="relu"
+            ),
             MaxPooling1D(pool_size=3),
             Flatten(),
-            Dense(10, activation="relu", kernel_initializer='random_normal'),
+            Dense(
+                10,
+                activation="relu"
+            ),
             Dropout(0.5),
-            Dense(1, activation='sigmoid', kernel_initializer='random_normal')
+            Dense(
+                1,
+                activation="sigmoid"
+            )
         ]
     )
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+def train(X, y, embeddings, model_params, test_size=0.25):
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, stratify=y, test_size=test_size)
+    vocab_size, embeddings_size = embeddings.shape
+    model_params["max_length"] = X.shape[1]
+    model_params["embeddings_size"] = embeddings_size
+    model_params["vocab_size"] = vocab_size
+
+    model = create_model(embeddings, model_params)
+
     # Tensorboard callback
     base_logdir = "scalars/{}_{}".format(MODEL_NAME, VERSION)
     logdir = os.path.join(LOGS_FOLDER, base_logdir)
     callback_tensorboard = TensorBoard(log_dir=logdir)
-    
+
     epochs = model_params["epochs"]
     batch_size = model_params["batch_size"]
     history = model.fit(
         X_train,
         y_train,
-        epochs=epochs, 
+        epochs=epochs,
         batch_size=batch_size,
         validation_data=(X_test, y_test),
         callbacks=[callback_tensorboard]
@@ -94,11 +140,12 @@ def train(X, y, embeddings, model_params, test_size=0.25):
 
     return model, history
 
+
 def save_model(model):
     saved_model_name = "{}_{}.json".format(MODEL_NAME, VERSION)
     model_json = model.to_json()
     with open(saved_model_name, "w") as json_file:
-        json_file.write(model_json)    
+        json_file.write(model_json)
 
 
 def load_model(model_filename):
@@ -106,7 +153,8 @@ def load_model(model_filename):
         model_json = json_file.read()
         model = model_from_json(model_json)
 
-    return model    
+    return model
+
 
 if __name__ == "__main__":
     dataset_file = None
@@ -116,18 +164,23 @@ if __name__ == "__main__":
             dataset_file = sys.argv[i+1]
         elif arg == "--embeddings":
             embedding_file = sys.argv[i+1]
-    
+
     if (dataset_file is not None) and (embedding_file is not None):
         print("=> Loading Dataset")
         data = load_dataset(dataset_file)
         print("=> Loading Embeddings")
-        dictionary, reversed_dictionary, embeddings = load_embeddings(embedding_file)
+        dictionary, reversed_dictionary, embeddings = \
+            load_embeddings(embedding_file)
         print("=> Processing data")
         X, y = process_data(data, dictionary)
         print("=> Training model")
         model_params = {
             "epochs": 50,
-            "batch_size": 32
+            "batch_size": 32,
+            "n_filters_1": 32,
+            "n_filters_2": 32,
+            "kernel_size_1": 4,
+            "kernel_size_2": 4
         }
         model, history = train(X, y, embeddings, model_params)
         save_model(model)
